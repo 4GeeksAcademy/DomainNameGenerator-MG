ebf93f307293136450eae516d70e4f0335f91bcd
Manuel Garay <84086007+uykarma@users.noreply.github.com>
2023-10-19 00:51:10 +0000

    [modificaciones en los loop]

--- src/app.js
+++ src/app.js
@@ -1,14 +1,13 @@
-console.log("Hola desde app.js!");
-
+console.log("Dominios generado mediante anidado de loops");
 let pronoun = ["the", "our"];
 let adj = ["great", "big"];
 let noun = ["jogger", "racoon"];
 let domain = [".com", ".net", ".us", ".es", ".io", ".ir", ".de", ".uy"];
 
-for (let d in domain) {
-  for (let p in pronoun) {
-    for (let a in adj) {
-      for (let n in noun) {
+for (const d in domain) {
+  for (const p in pronoun) {
+    for (const a in adj) {
+      for (const n in noun) {
         console.log(pronoun[p] + "" + adj[a] + "" + noun[n] + "" + domain[d]);
       }
     }
